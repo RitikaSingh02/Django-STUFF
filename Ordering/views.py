699b9fe62cf1e2aea74a5c63d76b16b4975974be
RitikaSingh02 <ritika2002singh@gmail.com>
2021-04-24 15:45:57 +0530

    added formset view rendering

--- Ordering/views.py
+++ Ordering/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from .forms import PizzaForm
+from .forms import PizzaForm, MultiplePizzaform
+from django.forms import formset_factory
 
 
 def home(request):
@@ -7,6 +8,7 @@ def home(request):
 
 
 def order(request):
+    multiple_form = MultiplePizzaform()
     if request.method == "POST":
         filled_form = PizzaForm(request.POST, request.FILES)
         print(filled_form)  # full form html
@@ -16,8 +18,30 @@ def order(request):
                                                                filled_form.cleaned_data['topping2'],
                                                                filled_form.cleaned_data['topping3'])
             new_form = PizzaForm()
-            return render(request, 'order.html', {'pizzaform': new_form, 'note': note})
+            return render(request, 'order.html', {'pizzaform': new_form, 'note': note, 'multiple_form': multiple_form})
     else:
         form = PizzaForm()
-        return render(request, 'order.html', {'pizzaform': form})
+        return render(request, 'order.html', {'pizzaform': form, 'multiple_form': multiple_form})
 # Create your views here.
+
+
+def pizzas(request):
+    number_pizza = 2
+    filled_multiple_form = MultiplePizzaform(request.GET)
+    if filled_multiple_form.is_valid():
+        number_pizza = filled_multiple_form.cleaned_data['number']
+    PizzaFormSet = formset_factory(PizzaForm, extra=number_pizza)
+    print(PizzaFormSet)
+    formset = PizzaFormSet()
+    if request.method == 'POST':
+        filled_formset = PizzaFormSet(request.POST)
+        print(filled_formset)
+        if filled_formset.is_valid():
+            for form in filled_formset:
+                print(form.cleaned_data['topping1'])
+            note = 'pizzas have been ordered'
+        else:
+            note = 'Order was not created , please try again'
+        return render(request, 'pizzas.html', {'note': note, 'formset': formset})
+    elif request.method == 'GET':
+        return render(request, 'pizzas.html', {'formset': formset})
